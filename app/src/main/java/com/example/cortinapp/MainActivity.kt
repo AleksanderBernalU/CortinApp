ff827353ad5ce27c734d52b2139edf35bb684a36
Aleksander <aleksander.bernalu@gmail.com>
2021-11-18 20:31:11 -0500

    actualizacion con navigation drawer

--- app/src/main/java/com/example/cortinapp/MainActivity.kt
+++ app/src/main/java/com/example/cortinapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.cortinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onLogin(view: View){
+        val intento = Intent(this,DrawerActivity::class.java)
+        startActivity(intento)
     }
 }
