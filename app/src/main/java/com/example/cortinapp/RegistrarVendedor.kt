29d2359d27b01556e55610e49ee22935b84a7741
andresR <anrioco2000@gmail.com>
2021-11-20 15:55:54 -0500

    navigation drawer

--- app/src/main/java/com/example/cortinapp/RegistrarVendedor.kt
+++ app/src/main/java/com/example/cortinapp/RegistrarVendedor.kt
@@ -2,10 +2,22 @@ package com.example.cortinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class RegistrarVendedor : AppCompatActivity() {
+class RegistrarVendedor : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registrar_vendedor)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val fragmento = inflater.inflate(R.layout.activity_registrar_vendedor, container, false)
+        return fragmento
     }
 }
