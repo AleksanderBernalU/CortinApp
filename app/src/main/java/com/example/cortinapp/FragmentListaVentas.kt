81f5121730d4c448a6aff096b529905801a4c990
May-da <90736804+May-da@users.noreply.github.com>
2021-11-24 16:02:58 -0500

    Implementación del RecycleView

---
+++ app/src/main/java/com/example/cortinapp/FragmentListaVentas.kt
@@ -0,0 +1,84 @@
+package com.example.cortinapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentListaVentas.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentListaVentas : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val fragmento: View = inflater.inflate(R.layout.fragment_lista_ventas, container, false)
+
+        return fragmento
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            val recyclerVentaList: RecyclerView = view.findViewById(R.id.recycleListaVenta)
+            var datos: ArrayList<Task> = ArrayList()
+            datos.add(Task(1, "09:00", 300000))
+            datos.add(Task(2, "12:00", 450000))
+            datos.add(Task(3, "18:00", 1000000))
+            datos.add(Task(4, "18:00", 5070000))
+            var taskAdapter = TaskAdapter(datos){
+                val datos = Bundle()
+                datos.putInt("Codigo", it.codigo)
+                datos.putString("Hora", it.time)
+                datos.putInt("Venta Total", it.precio)
+                Navigation.findNavController(view).navigate(R.id.nav_detail, datos)
+            }
+            recyclerVentaList.setHasFixedSize(true)
+            recyclerVentaList.adapter = taskAdapter
+            recyclerVentaList.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FragmentListaVentas.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+                FragmentListaVentas().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+    }
+}
---
+++ app/src/main/java/com/example/cortinapp/TaskAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.cortinapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskAdapter(private val datos: ArrayList<Task>, private val clickListener : (Task) ->  Unit) :
+    RecyclerView.Adapter<TaskAdapter.TaskViewHolder>(){
+    class TaskViewHolder(val layout: View) : RecyclerView.ViewHolder(layout)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val layout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.task_list_item, parent, false)
+        return TaskViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val task = datos[position]
+        val textViewCodigo: TextView = holder.layout.findViewById(R.id.textViewCodigo)
+        val textViewTime: TextView = holder.layout.findViewById(R.id.textViewTime)
+        val textViewPercio: TextView = holder.layout.findViewById(R.id.textViewPrecio)
+        textViewCodigo.text = task.codigo.toString()
+        textViewTime.text = task.time
+        textViewPercio.text = task.precio.toString()
+        holder.layout.setOnClickListener{ clickListener(task)};
+    }
+
+    override fun getItemCount(): Int {
+        return datos.size
+    }
+
+
+}
+
+
